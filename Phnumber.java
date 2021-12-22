eb15938612a415d324e328051f3cd750e30678c9
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-22 22:32:23 +0530

    String array programme using for loop

---
+++ Phnumber.java
@@ -0,0 +1,8 @@
+class Phnumber{
+     public static void main(String []args){
+       long []cntnumber = {8147214046l,7892721595l,9842736775l,8123019051l,9731008824l,8296796488l};
+          for(int index=0; index< cntnumber.length; index= index+1){
+               System.out.println(cntnumber[index]);
+          }
+     }
+}
---
+++ Year.java
@@ -0,0 +1,8 @@
+class Year{
+    public static void main(String []args){
+      String []months ={"january","february","march","april","may","june","july","august","september","october","november","december"};
+           for(int index=0; index< months.length; index = index+1){
+               System.out.println(months[index]);
+           }
+    }
+}
